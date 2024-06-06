b3e93b2dbf6e4bf045bf3a110a56295fcfebb874
Isabela Ginglass <gaabiscuit@gmail.com>
2024-06-06 19:35:52 -0300

    First Comit
    upload files

---
+++ index.js
@@ -0,0 +1,44 @@
+class MobileNavbar {
+    constructor(mobileMenu, headerListContainer, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.headerListContainer = document.querySelector(headerListContainer);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.headerListContainer.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".header-list-container",
+    ".header-list-container li",
+  );
+  mobileNavbar.init();
